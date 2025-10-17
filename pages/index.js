e071c3224191caa2917b99e9ee1c6da6e744d58e
soporteclubnautilus <soporteclubnautilus@gmail.com>
2025-10-17 14:02:58 -0300

    Crear el archivo pages/index.js con contenido inicial

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      fontSize: "2rem"
+    }}>
+      ¡Próximamente!
+    </div>
+  );
+}
