278d54826f453ff3b549cad53de7ae816af21204
jorge <jorgeturina@gmail.com>
2025-10-17 21:07:43 -0300

    eliminacion

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const isGithubPages = process.env.GITHUB_PAGES === 'true';
-
-module.exports = {
-  output: 'export',
-  basePath: isGithubPages ? '/soporteclubnautilus.github.io' : '',
-  assetPrefix: isGithubPages ? '/soporteclubnautilus.github.io/' : '',
-};
